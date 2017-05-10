4830739b938a9fa98136ab545708155a252e895a
Garritt <gmoede@wisc.edu>
2017-05-09 23:04:37 -0500

    configuration class

--- src/model.py
+++ src/model.py
@@ -1,15 +1,133 @@
-import dataset as ds
+# Main Classification model script with Configuration Class - Garritt Moede #
+
+# utilities
 import gensim 
 from gensim.utils import simple_preprocess
 import numpy as np
 import math
 import pandas as pd
-# import pydot
+from concurrent.futures import ThreadPoolExecutor
+
+# keras shit
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.utils import plot_model
 from keras.preprocessing import sequence
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
+
+# threading bug.. 
+import tensorflow as tf
+
+
+
+# 	- Configuration -
+#
+# takes epochs and other model global params
+# takes layer array of tuples that specifies the order of layers in the network
+#
+#	example: layers = [(Embedding, embedding_weights, input_length),
+#					   (LSTM, total_cells_in_layer, optional:dropout_rate), 
+#					   (Dense, 50), (Dense, 1)]  
+
+class Configuration(object):
+
+	def __init__(self, name, batchsize, epochs, layers):
+		self.name = name
+		self.batchsize = batchsize
+		self.epochs = epochs
+		self.layers = layers
+		self.model = Sequential()
+		for layer in self.layers:
+			if layer[0] == 'Embedding':
+				weights = layer[1]
+				self.model.add(Embedding(input_dim=weights.shape[0], output_dim=weights.shape[1], input_length=layer[2], weights=[weights]))
+
+			units = layer[1]
+			try:
+				dropout = layer[2]
+			except IndexError:
+				dropout = 0	
+
+			if layer[0] == 'LSTM':
+				self.model.add(LSTM(units, dropout=dropout, recurrent_dropout=dropout)) 
+			elif layer[0] == 'Dense':
+				self.model.add(Dense(units, activation='sigmoid'))
+			## Other Layers ?? ##
+
+		self.graph = tf.get_default_graph() 	# used for multithreading hack. see https://github.com/fchollet/keras/issues/2397
+	
+
+	def run(self, lossf, optimizef, TrX, TrY):
+		# Do early stopping and ModelCheckpoint callback
+		checkpoint = ModelCheckpoint('best_weights.hdf5', monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+		early = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=3, verbose=0, mode='auto')
+		callbacks = [checkpoint, early]
+		self.model.compile(loss=lossf, optimizer=optimizef, metrics=['accuracy'])
+		# train
+		self.model.fit(TrX, TrY, epochs=self.epochs, batch_size=self.batchsize, callbacks=callbacks, validation_split=.25)
+		
+
+	def accuracy(self, TsX,  TsY):
+		#write = open('../trials/'+self.name+'.txt')
+
+		self.model.load_weights('best_weights.hdf5')
+		scores = self.model.evaluate(TsX, TsY, verbose=1)
+		# write.write(' ', self.name, '\n')
+		# write.write(' -- epochs: ', self.epochs)
+		# write.write(' -- batchsize: ', self.batchsize)
+		# write.write(' -- layers: ', end='')
+		# for layer in self.layers:
+		# 	write.write(layer, ' -> ' ,end='')
+		# write.write('\n -- test accuracy: ', scores[1])
+		print('\n ---- ', self.name, ' ----\n')
+		print(' -- epochs: ', self.epochs)
+		print(' -- batchsize: ', self.batchsize)
+		print(' -- layers: ', end='')
+		for layer in self.layers:
+			if layer[0] is 'Embedding':
+				print('Embedding (Input) -> ', end='')
+			else:
+				print(layer, ' -> ', end='')
+		print('Output')
+		print(' -- test accuracy: ', scores[1], '\n')
+
+	# Not currently working with keras and tensorflow
+	def run_worker(self, Xt, Yt, Xts, Yts):
+		with tf.Session(graph=self.graph):
+			self.run('mean_squared_error', 'adam', Xt, Yt)
+			self.accuracy(Xts, Yts)
+
+
+
+def dataprep(df):
+	lengths = []
+	tweet_vecs = []
+	outputs = []
+	for index,row in df.iterrows():
+		tokens = row[6].split(" ")
+		lengths.append(len(tokens))
+		vec = []
+		for word in tokens:
+			try:
+				idx = w2v.wv.vocab[word].index
+				vec.extend([idx])							
+			# zero padding
+			except TypeError:								
+				pass															
+			# word not it dict, simply ignore
+			except KeyError:
+				pass
+
+		if (row[1] == 4):
+			outputs.append(1)
+		else:
+			outputs.append(row[1])
+		
+		tweet_vecs.append(vec)
+	return (tweet_vecs, outputs, lengths)
+
 
 
 # Load W2V Model #
@@ -21,114 +139,58 @@ dimension = w2v.vector_size
 df_train = pd.read_csv('../data/sampleTrain.csv', encoding='ISO-8859-1')
 df_test = pd.read_csv('../data/sampleTest.csv', encoding='ISO-8859-1')
 
+
 # Testing data prep #
-tweet_vecs_test = []
-outputs_test = []
-for index,row in df_test.iterrows():
-	tokens = row[5].split(" ")
-	vec = []
-	for word in tokens:
-		try:
-			idx = w2v.wv.vocab[word].index
-			vec.extend([idx])							
-		# zero padding
-		except TypeError:
-			vec.extend([0])											
-		# word not it dict
-		except KeyError:
-			vec.extend([0])
-
-	if (row[0] == 4):
-		outputs_test.append(1)
-	else:
-		outputs_test.append(row[0])
-	
-	tweet_vecs_test.append(vec)
-
-tweet_vecs_test = np.array(tweet_vecs_test)
+data = dataprep(df_test)
+tweet_vecs_test = data[0]
+outputs_test = data[1]
+# lengths = data[2]
 
 # Training data prep
-tweet_vecs_train = []
-outputs_train = []
-lengths = []
-for index,row in df_train.iterrows():
-	tokens = row[6].split(" ")
-	lengths.append(len(tokens))
-	vec = []
-
-	for word in tokens:	
-		try:
-			idx = w2v.wv.vocab[word].index
-			vec.extend([idx])
-			
-																	##  CURRENTLY PUSHING INDEX OF WORD EMBEDDING VECTOR INSTEAD OF VECTOR ITSELF. (EACH IDX REPRESENTS)
-																	##  This assumes that the embedding layer is functioning by mapping the index that it is fed to a wordvector
-																	##  This change fixed the following error:
-																	# 	
-																	#    InvalidArgumentError (see above for traceback): indices[0,8] = -1 is not in [0, 135731)
-																	#	 [[Node: embedding_1/Gather = Gather[Tindices=DT_INT32, Tparams=DT_FLOAT, validate_indices=true, _device="/job:localhost/replica:0/task:0/cpu:0"](embedding_1/embeddings/read, _recv_embedding_1_input_0)]]
-
-
-		# zero padding
-		except TypeError:
-			vec.extend([0])											# This should not be zero! it is supposed to map to a vector that represnts a any word not in the dict
-																	# Look to see if it is possible to add a ceritan word to the model in gensim?
-			# vec.extend(np.zeros(dimension))
-		# word not it dict
-		except KeyError:
-			vec.extend([0])
-			# vec.extend(np.zeros(dimension))
-
-	if (row[1] == 4):												# Hacky. Will move later
-		outputs_train.append(1)
-	else:
-		outputs_train.append(row[1])
-	
-	tweet_vecs_train.append(vec)
-
-
-
+data = dataprep(df_train)
+tweet_vecs_train = data[0]
+outputs_train = data[1]
+lengths = data[2]
 
 
 
 # train and test data format #
 mean = sum(lengths)/len(lengths)
 std = np.std(lengths) 
-time_sequence = math.ceil(mean + (2*std))										# Constant for both train and test sets. Evaluated from training set examples
+time_sequence = math.ceil(mean + (3*std))										# Constant for both train and test sets. Evaluated from training set examples
 tweet_vecs_train = np.array(tweet_vecs_train)
 
 X_train = sequence.pad_sequences(tweet_vecs_train, maxlen=time_sequence) 		# Fixed by downgrading to numpy 1.11.2 
 X_test = sequence.pad_sequences(tweet_vecs_test, maxlen=time_sequence)
 weights = np.load(open('../vocab_weights', 'rb'))
 
+# create different configs based on LSTM Units #
+confs = []
+#c = Configuration('CONF1', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 100, .2), ('Dense', 1)])
+confs.append(Configuration('CONF1', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 100, .2), ('Dense', 1)]))
+confs.append(Configuration('CONF2', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 50, .2), ('Dense', 1)]))
+confs.append(Configuration('CONF2', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 1, .2), ('Dense', 1)]))
+
+# create different configs based on Dense Units
+confs.append(Configuration('CONF2', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 50, .2), ('Dense', 10)]))
+confs.append(Configuration('CONF2', 60, 2, [('Embedding', weights, time_sequence), ('LSTM', 50, .2), ('Dense', 10), ('Dense', 1)]))
+
+# Run inline 
+for c in confs:
+	c.run('mean_squared_error', 'adam', X_train, outputs_train)
+	c.accuracy(X_test, outputs_test)
 
 
-#print(time_sequence, weights.shape[1])
+# Threading #
+# -- does not currently work
 
-# create Model #
-model = Sequential()
-embed = Embedding(name="inpt", input_dim=weights.shape[0], output_dim=weights.shape[1], input_length=time_sequence, weights=[weights])
-print(embed)
-model.add(embed) # input_length=time_sequence, 
-model.add(LSTM(1, dropout=0.2, recurrent_dropout=0.2)) # input_shape=(None, weights.shape[0], weights.shape[1])) 
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-
-
-#intermediate_layer_model = Model(inputs=model.input,
-#                                outputs=model.get_layer('inpt').output)
-#intermediate_output = intermediate_layer_model.predict(X_train)
+# with ThreadPoolExecutor(max_workers=4) as e:
+# 	for c in confs:
+# 		result = e.submit(c.run_worker, X_test, outputs_test, X_test, outputs_test)
 
 
 
-print("\n ---- fitting model ---- \n")
 
-# Fit and Train Model #
-model.fit(X_train, outputs_train, epochs=2, batch_size=60)
-# Evaluate #
-scores = model.evaluate(X_test, outputs_test, verbose=1)
-print(scores)
 
 
 
